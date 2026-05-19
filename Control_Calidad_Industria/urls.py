3965f7dc77e6acc7e1244b2c87932a624a882e5c
josuresano <josu.resano@opendeusto.es>
2026-05-19 21:32:17 +0200

    Registro de rutas de JWT en urls.py

--- Control_Calidad_Industria/urls.py
+++ Control_Calidad_Industria/urls.py
@@ -16,12 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Gestion_NC import views
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from Gestion_NC import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', views.inicio, name='inicio'),
     path('nc/', views.lista_nc, name='lista_nc'),
     path('acciones/', views.lista_acciones, name='lista_acciones'),
